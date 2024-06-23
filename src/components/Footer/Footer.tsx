ff71da02aaec0fd5f7add0b0fbcbf7b188c7d94c
DenisOsetrov <dvosetrov@gmail.com>
2024-06-23 13:55:47 +0300

    final project - that is all!

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,22 @@ import './Footer.css';
 
 const Footer: React.FC = () => (
     <footer className="footer">
-        <p>© 2024 Movie App. All rights reserved.</p>
+        <div className="footer-content">
+            <div className="footer-section company-info">
+                <h4>Okten School Production</h4>
+                <p>Subdivision: January-2024</p>
+            </div>
+            <div className="footer-section developer-info">
+                <h4>Developer</h4>
+                <p>Osetrov Denis Volodimirovich</p>
+                <ul>
+                    <li>Email: <a href="mailto:dvosetrov@gmail.com">dvosetrov@gmail.com</a></li>
+                    <li>Phone: <a href="tel:+380982913157">0982913157</a></li>
+                    <li>Nickname: <a href="https://t.me/denfree777" target="_blank" rel="noopener noreferrer">https://t.me/denfree777</a></li>
+                </ul>
+            </div>
+        </div>
+        <p className="footer-bottom">© 2024 Movie App. All rights reserved.</p>
     </footer>
 );
 
